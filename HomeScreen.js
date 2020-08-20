a73b38551aa33da60ec0234cfc07e3bd00a048e1
bkouchi <bkouchi@users.noreply.github.com>
2020-08-20 11:59:24 -0700

    Add Button to HomeScreen

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>You have (undefined) friends.</Text>
+
+        <Button
+          title="Add some friends"
+          onPress={() =>
+            this.props.navigation.navigate('Friends')
+          }
+        />
       </View>
     );
   }
