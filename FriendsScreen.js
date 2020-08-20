493d832eb25f30367d78db695fcad3cf2d0e4291
bkouchi <bkouchi@users.noreply.github.com>
2020-08-20 12:56:49 -0700

    Display possibleFriends on FriendsScreen

--- FriendsScreen.js
+++ FriendsScreen.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { FriendsContext } from './FriendsContext';
 
 class FriendsScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Add friends here!</Text>
+
+        {
+          this.context.possibleFriends.map((friend, index) => (
+            <Button
+              key={ friend }
+              title={ `Add ${ friend }` }
+              onPress={() =>
+                this.context.addFriend(index)
+              }
+            />
+          ))
+        }
 
         <Button
           title="Back to home"
@@ -17,6 +30,7 @@ class FriendsScreen extends React.Component {
     );
   }
 }
+FriendsScreen.contextType = FriendsContext;
 
 const styles = StyleSheet.create({
   container: {
