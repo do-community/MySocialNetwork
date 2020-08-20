e6b95f897d34b48130cc2dc4b40484564267a5cd
bkouchi <bkouchi@users.noreply.github.com>
2020-08-20 13:25:48 -0700

    Add friendsReducer to App.js

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import friendsReducer from './FriendsReducer';
 import HomeScreen from './HomeScreen';
 import FriendsScreen from './FriendsScreen';
+
+const store = createStore(friendsReducer);
 
 const Stack = createStackNavigator();
 
 class App extends React.Component {
   render() {
     return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name="Friends"
-            component={FriendsScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+            />
+            <Stack.Screen
+              name="Friends"
+              component={FriendsScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     );
   }
 }
