c06835ea229ef6d6b54e756e27a8506842e04d68
bkouchi <bkouchi@users.noreply.github.com>
2020-08-20 12:01:02 -0700

    Add Button to FriendsScreen

--- FriendsScreen.js
+++ FriendsScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class FriendsScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Add friends here!</Text>
+
+        <Button
+          title="Back to home"
+          onPress={() =>
+            this.props.navigation.navigate('Home')
+          }
+        />
       </View>
     );
   }
