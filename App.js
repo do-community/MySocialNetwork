0b163226963f94d228575f5ab91b6817aa70ba7b
bkouchi <bkouchi@users.noreply.github.com>
2020-08-20 12:04:01 -0700

    Add possibleFriends, currentFriends, and addFriend

--- App.js
+++ App.js
@@ -9,6 +9,37 @@ import FriendsScreen from './FriendsScreen';
 const Stack = createStackNavigator();
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      possibleFriends: [
+        'Alice',
+        'Bob',
+        'Sammy',
+      ],
+      currentFriends: [],
+    }
+  }
+
+  addFriend = (index) => {
+    const {
+      currentFriends,
+      possibleFriends,
+    } = this.state
+
+    // Pull friend out of possibleFriends
+    const addedFriend = possibleFriends.splice(index, 1)
+
+    // And put friend in currentFriends
+    currentFriends.push(addedFriend)
+
+    // Finally, update the app state
+    this.setState({
+      currentFriends,
+      possibleFriends,
+    })
+  }
+
   render() {
     return (
       <NavigationContainer>
