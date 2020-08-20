fc2e068a9021b2692c89a5af59b81642850f8e5f
bkouchi <bkouchi@users.noreply.github.com>
2020-08-20 13:21:50 -0700

    Add ADD_FRIEND

--- FriendsReducer.js
+++ FriendsReducer.js
@@ -11,6 +11,27 @@ const INITIAL_STATE = {
 
 const friendsReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'ADD_FRIEND':
+      // Pulls current and possible out of previous state
+      // We do not want to alter state directly in case
+      // another action is altering it at the same time
+      const {
+        current,
+        possible,
+      } = state;
+
+      // Pull friend out of friends.possible
+      // Note that action.payload === friendIndex
+      const addedFriend = possible.splice(action.payload, 1);
+
+      // And put friend in friends.current
+      current.push(addedFriend);
+
+      // Finally, update the redux state
+      const newState = { current, possible };
+
+      return newState;
+
     default:
       return state
   }
